9eecebba0530a956f5927f5f771957ac93276c3a
Suryasis <suryasishirokpaul@gmail.com>
2021-12-07 13:02:34 +0530

    Adding the list of html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>To Do list</h1>`,
+  template: `
+  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+  <label for="vehicle1"> Task 1 </label><br>
+  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+  <label for="vehicle2"> Task 2 </label><br>
+  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <label for="vehicle3"> Task 3 </label><br><br>`
 })
 export class AppComponent  { name = 'World'; }
