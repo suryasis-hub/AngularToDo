a777c86b03ced21855bfd730692e9ba3f4994f55
Suryasis <suryasishirokpaul@gmail.com>
2021-12-07 13:08:01 +0530

    Assigning correct task names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
-  <label for="vehicle1"> Task 1 </label><br>
-  <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
-  <label for="vehicle2"> Task 2 </label><br>
-  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+  <input type="checkbox" id="task1" name="task1" value="Task1">
+  <label for="task1"> Task 1 </label><br>
+  <input type="checkbox" id="task2" name="task2" value="Task2">
+  <label for="task2"> Task 2 </label><br>
+  <input type="checkbox" id="task3" name="task3" value="Task3">
   <label for="vehicle3"> Task 3 </label><br><br>`
 })
 export class AppComponent  { name = 'World'; }
