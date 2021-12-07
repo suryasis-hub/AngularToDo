e305f639337eae7dc4e549963a817def7f79d6f9
Suryasis <suryasishirokpaul@gmail.com>
2021-12-07 18:49:25 +0530

    Adding code to move html to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <input type="checkbox" id="task1" name="task1" value="Task1">
-  <label for="task1"> Task 1 </label><br>
-  <input type="checkbox" id="task2" name="task2" value="Task2">
-  <label for="task2"> Task 2 </label><br>
-  <input type="checkbox" id="task3" name="task3" value="Task3">
-  <label for="task3"> Task 3 </label><br><br>`
+  templateUrl:'./app.component.html'
 })
 export class AppComponent  { name = 'World'; }
